477c5b9725b651dc1715248989f3afefe5549730
unknown <giroyrommel03@gmail.com>
2020-12-24 23:28:04 +0800

    Initial commit

---
+++ frmMain.Designer.cs
@@ -0,0 +1,113 @@
+﻿
+namespace WinFormUI
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cmbChoice = new System.Windows.Forms.ComboBox();
+            this.txtTextArea = new System.Windows.Forms.TextBox();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cmbChoice
+            // 
+            this.cmbChoice.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.cmbChoice.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbChoice.FormattingEnabled = true;
+            this.cmbChoice.Items.AddRange(new object[] {
+            "Male",
+            "Female"});
+            this.cmbChoice.Location = new System.Drawing.Point(19, 21);
+            this.cmbChoice.Name = "cmbChoice";
+            this.cmbChoice.Size = new System.Drawing.Size(484, 27);
+            this.cmbChoice.TabIndex = 0;
+            // 
+            // txtTextArea
+            // 
+            this.txtTextArea.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTextArea.Location = new System.Drawing.Point(19, 63);
+            this.txtTextArea.Multiline = true;
+            this.txtTextArea.Name = "txtTextArea";
+            this.txtTextArea.Size = new System.Drawing.Size(484, 279);
+            this.txtTextArea.TabIndex = 0;
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Location = new System.Drawing.Point(19, 358);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(100, 32);
+            this.btnStart.TabIndex = 2;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnPause.Location = new System.Drawing.Point(125, 358);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(100, 32);
+            this.btnPause.TabIndex = 3;
+            this.btnPause.Text = "Pause";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 405);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.txtTextArea);
+            this.Controls.Add(this.cmbChoice);
+            this.Font = new System.Drawing.Font("Tahoma", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Text To Speech";
+            this.Load += new System.EventHandler(this.frmMain_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox cmbChoice;
+        private System.Windows.Forms.TextBox txtTextArea;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+    }
+}
---
+++ frmMain.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Synthesis;
+
+namespace WinFormUI
+{
+    public partial class frmMain : Form
+    {
+        SpeechSynthesizer synthesizer = new SpeechSynthesizer();
+        public frmMain()
+        {
+            InitializeComponent();
+            cmbChoice.SelectedIndex = 0;  
+           
+        }
+
+        private void frmMain_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void Initialize(string textArea)
+        {
+          
+            synthesizer.SetOutputToDefaultAudioDevice();
+            synthesizer.SpeakAsync(textArea);
+ 
+        }
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            if (btnStart.Text == "Start")
+            { 
+                Initialize(txtTextArea.Text);
+                btnStart.Text = "Stop"; 
+            }
+            else if (btnStart.Text == "Stop")
+            {
+                synthesizer.SpeakAsyncCancelAll();
+                btnStart.Text = "Start";
+            }
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            if (btnPause.Text == "Pause")
+            {
+                synthesizer.Pause();
+                btnPause.Text = "Resume";
+            }
+            else if (btnPause.Text == "Resume")
+            {
+                synthesizer.Resume();
+                btnPause.Text = "Pause";
+            }
+        }
+    }
+}
